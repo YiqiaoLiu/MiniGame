fa527dc32236664b4a3d134c8f1b01e20d55dd70
YiqiaoLiu <yiqiao.liu@duke.edu>
2016-10-29 10:46:06 -0400

    Register and create a window

--- Win32_MiniGame/Win32_MiniGame/windowTest.cpp
+++ Win32_MiniGame/Win32_MiniGame/windowTest.cpp
@@ -1,2 +1,104 @@
 #include <windows.h>
 
+// Function handles all the message created by the window
+LRESULT CALLBACK WindowProcOfMiniGame(
+	HWND   Window,
+	UINT   Msg,
+	WPARAM wParam,
+	LPARAM lParam) 
+{
+	LRESULT callbackResult = 0;
+
+	// Define the message handler
+	switch (Msg)
+	{
+	case WM_QUIT:
+		OutputDebugStringA("WM_QUIT message received\n");
+		break;
+
+	case WM_SIZE:
+		OutputDebugStringA("WM_SIZE message received\n");
+		break;
+
+	case WM_CREATE:
+		OutputDebugStringA("WM_CREATE message received\n");
+		break;
+
+	case WM_DESTROY:
+		OutputDebugStringA("WM_DESTROY message received\n");
+		break;
+
+	case WM_ACTIVATEAPP:
+		OutputDebugStringA("WM_ACTIVATEAPP message received\n");
+		break;
+
+	default:
+		callbackResult = DefWindowProc(Window, Msg, wParam, lParam);
+		break;
+	}
+
+	return callbackResult;
+
+}
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nCmdShow) 
+{
+
+	// Avoid un-using warning
+	UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+
+	//MessageBoxA(0, "This is the mini game", "Mini_Game", MB_OK | MB_ICONINFORMATION);
+	WNDCLASS windowsClass = {0};
+	// Setting the window's features
+    windowsClass.style = CS_HREDRAW | CS_VREDRAW;						// Set the window's type
+	windowsClass.lpfnWndProc = WindowProcOfMiniGame;					// Pointer to the window procedure
+	windowsClass.hInstance = hInstance;									// Set the instance of the window
+//	HICON     hIcon;
+//	HBRUSH    hbrBackground;
+//	LPCTSTR   lpszMenuName;
+	windowsClass.lpszClassName = "MiniGameWindow";						// The name of the window class
+
+	// Register the window class
+	if (RegisterClass(&windowsClass)) {
+		// Create the window
+		HWND windowHandle = CreateWindowEx(
+			0,
+			windowsClass.lpszClassName,
+			"MiniGame",
+			WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			0,
+			0,
+			hInstance,
+			0);
+
+		if (windowHandle) {
+			for (;;) {
+				MSG Message;
+				BOOL messageResult = GetMessage(&Message, 0, 0, 0);
+				if (messageResult > 0) {
+					TranslateMessage(&Message);
+					DispatchMessage(&Message);
+				}
+				else {
+					break;
+				}
+			}
+		}
+		else {
+			OutputDebugStringA("Creating window failure!\n");
+		}
+	}
+	else {
+		OutputDebugStringA("Registering window failure!\n");
+	}
+	return 0;
+}
